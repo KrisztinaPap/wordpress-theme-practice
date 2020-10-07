a67e88980220ca056f756264ad8bd2d67b9db29c
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-07 13:50:07 -0600

    Adds scripts in enqueue action;

--- functions.php
+++ functions.php
@@ -7,7 +7,16 @@ register_nav_menus(
     )
 );
 
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_script( 
+        'techcareers-theme-scripts',
+        get_theme_file_uri( '/assets/js/scripts.js' ),
+        array(), // Dependencies
+        strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ), // <--- Version number (based on time to fight broswer cache!)
+        FALSE // Should it output in the wp_footer()?
+    );
 
+} );
 
 
 ?>
