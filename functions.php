44f70a8e04d482f93d2d9b37c14210aa5ca95ed0
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-07 14:04:48 -0600

    Enqueues main style sheet;

--- functions.php
+++ functions.php
@@ -9,13 +9,18 @@ register_nav_menus(
 
 add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_script( 
-        'techcareers-theme-scripts',
-        get_theme_file_uri( '/assets/js/scripts.js' ),
+        'techcareers-theme-scripts', // A name for the file
+        get_theme_file_uri( '/assets/js/scripts.js' ), // Public path to the script file
         array(), // Dependencies
         strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ), // <--- Version number (based on time to fight broswer cache!)
         FALSE // Should it output in the wp_footer()?
     );
-
+    wp_enqueue_style(
+        'techcareers-theme-main-styles',
+        get_theme_file_uri( '/assets/css/main.css' ), // Public path to the style file
+        strftime( get_theme_file_uri( '/assets/css/main.css' ) ), // <--- Version number (based on time to fight broswer cache!)
+        'all'
+    );
 } );
 
 
