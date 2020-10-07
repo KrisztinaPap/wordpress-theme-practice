1a1bcd2b181642ca2cf0c37ca87c37ccd3169175
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-07 11:12:30 -0600

    Adds footer;

---
+++ footer.php
@@ -0,0 +1,16 @@
+    <footer>
+        <h2><?php bloginfo( 'title' ); ?> Footer</h2>
+        <p>
+            &copy;
+            <?php echo date (' Y' ); ?>
+            <a href="<?php echo siteurl(); ?>">
+                <?php bloginfo( 'title' ); ?>
+            </a>
+            All Rights Reserved.
+        </p>
+    </footer>
+    <?php
+        wp_footer();
+    ?>
+</body>
+</html>
